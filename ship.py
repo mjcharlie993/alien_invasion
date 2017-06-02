206d924364c2f94d15c6ea7a8cd6124d1c2d652f
mjcharlie993 <jiachen_mao@163.com>
2017-06-02 18:06:27 +0800

    Make ship move in four directions

--- ship.py
+++ ship.py
@@ -12,27 +12,37 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        # Start each new ship at the bottom  center of the screen.
+        # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.centery = self.screen_rect.centery
+#        self.rect.bottom = self.screen_rect.bottom
+        self.rect.center = self.screen_rect.center
 
         # Store a decimal value for the ship's center.
-        self.center = float(self.rect.centerx)
+        self.centerx0 = float(self.rect.centerx)
+        self.centery0 = float(self.rect.centery)
 
         # Movement flags.
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Update the ship's position, based on movement flags."""
         # Update the ship's center value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.center += self.ai_settings.ship_speed_factor
+            self.centerx0 += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
-            self.center -= self.ai_settings.ship_speed_factor
+            self.centerx0 -= self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top > 0:
+            self.centery0 -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.centery0 += self.ai_settings.ship_speed_factor
 
         # Update rect object from self.center.
-        self.rect.centerx = self.center
+        self.rect.centerx = self.centerx0
+        self.rect.centery = self.centery0
 
     def blitme(self):
         """Draw the ship at its current location."""
