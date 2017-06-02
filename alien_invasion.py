4c5aa2aba24e93cacfcb2a92a34b4300aad5b1db
mjcharlie993 <jiachen_mao@163.com>
2017-06-02 17:53:54 +0800

    Create a new game.

---
+++ alien_invasion.py
@@ -0,0 +1,29 @@
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+
+def run_game():
+
+    # Initialize pygame, settings, and screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(ai_settings, screen)
+
+    # Make a group to store bullets in.
+    bullets = Group()
+
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+run_game()
